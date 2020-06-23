e067738fee0bdd7b022a80458bea2e7ee7c8bb6a
Olga Gladiy <bel-olga@yandex.ru>
2020-06-23 13:15:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long costinKopecks = 3513025;
+        long miles = costinKopecks /100/20;
+        System.out.println(miles);
+    }
+}
